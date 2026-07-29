9e7398f58089483503e53ee0529b6ba6144ba637
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2026-07-29 21:56:09 +0300

    hello world

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from kotibotti!")
+import os
+
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
 
 
-if __name__ == "__main__":
-    main()
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f"Hello {update.effective_user.first_name}")
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+
+app.run_polling()
